567466528df325bd0ee58480b74e31c1171987a7
sames <samesin128@gmail.com>
2026-06-13 03:31:59 +0900

    initial commit

---
+++ keirin_morning.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d C:\Users\sames\Desktop\keirin_service
+echo [%date% %time%] 朝バッチ開始 >> keirin_log.txt
+python keirin_batch.py >> keirin_log.txt 2>&1
+python keirin_supabase_save.py >> keirin_log.txt 2>&1
+echo [%date% %time%] 朝バッチ完了 >> keirin_log.txt
